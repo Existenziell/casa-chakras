5c0d90d78d73ff8840dd53dffaee3047a2199892
Christof <bauer.christof@gmail.com>
2022-05-24 16:54:21 -0500

    Introduce Blob

--- pages/about.js
+++ pages/about.js
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import langEN from '../i18n/en.json'
 import langES from '../i18n/es.json'
 import Quote from '../components/Quote'
+import Blob from '../components/Blob'
 
 const About = ({ i18n }) => {
   return (
@@ -15,6 +16,10 @@ const About = ({ i18n }) => {
       <div className='px-4 md:px-8 py-24'>
         <h1 className='text-4xl md:text-6xl my-4'>{i18n.T1}</h1>
         <Quote text={i18n.T2} classes='mx-auto w-max mt-12 mb-16' />
+
+        <div className='hidden md:block'>
+          <Blob />
+        </div>
 
         <div className='grid grid-cols-1 md:grid-cols-2 gap-12 items-center'>
           <img src='/img/about/1.jpg' alt='about1' className='shadow-lg rounded' />
