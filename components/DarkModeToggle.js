ef3cc502768c5b85f30021a58e3e43a5f360d0c7
Christof <bauer.christof@gmail.com>
2022-05-24 17:05:56 -0500

    Adjust CSS

--- components/DarkModeToggle.js
+++ components/DarkModeToggle.js
@@ -27,7 +27,7 @@ const DarkModeToggle = () => {
   }
 
   return (
-    <div className='mt-4 mr-4'>
+    <div className='mt-6 mr-6'>
       {!darkMode ?
         <svg onClick={() => toggleDarkMode('dark')} xmlns='http://www.w3.org/2000/svg' className='h-10 w-10 hover:text-slate-400 cursor-pointer text-brand hover:scale-105' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
           <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z' />
